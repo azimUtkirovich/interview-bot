358497bc48e77beb736104cc51fc116148c95568
azimUtkirovich <azimutkirovich80@gmail.com>
2023-10-06 13:43:43 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,105 @@
+require("dotenv").config();
+const {
+  Bot,
+  Keyboard,
+  InlineKeyboard,
+  GrammyError,
+  HttpError,
+} = require("grammy");
+const { getRandomQuestion, getCorrectAnswer } = require("./utils");
+
+const bot = new Bot(process.env.BOT_API_KEY);
+
+bot.command("start", async (ctx) => {
+  const startKeyboard = new Keyboard()
+    .text("HTML")
+    .text("CSS")
+    .row()
+    .text("JavaScript")
+    .text("React")
+    .row()
+    .text("Случайный вопрос")
+    .resized();
+  await ctx.reply(
+    "Привет! Я - Frontend Interview Bot! 🤖 \nЯ помогу тебе подготовиться к интевью по фронтенду!"
+  );
+  await ctx.reply("С чего начнем? Выбери тему вопроса в меню 👇", {
+    reply_markup: startKeyboard,
+  });
+});
+
+bot.hears(["HTML", "CSS", "JavaScript", "React", "Случайный вопрос"], async (ctx) => {
+  const topic = ctx.message.text.toLowerCase();
+  const {question, questionTopic} = getRandomQuestion(topic);
+
+  let inLineKeyboard;
+
+  if (question.hasOptions) {
+    const buttonRows = question.options.map((option) => [
+      InlineKeyboard.text(
+        option.text,
+        JSON.stringify({
+          type: `${questionTopic}-option`,
+          isCorrect: option.isCorrect,
+          questionId: question.id,
+        })
+      ),
+    ]);
+
+    inLineKeyboard = InlineKeyboard.from(buttonRows);
+  } else {
+    inLineKeyboard = new InlineKeyboard().text(
+      "Узнать ответ",
+      JSON.stringify({ type: questionTopic, questionId: question.id,
+     })
+    );
+  }
+
+  await ctx.reply(question.text, {
+    reply_markup: inLineKeyboard,
+  });
+});
+
+bot.on("callback_query:data", async (ctx) => {
+  const callbackData = JSON.parse(ctx.callbackQuery.data);
+
+  if (!callbackData.type.includes("-option")) {
+    const answer = getCorrectAnswer(callbackData.type, callbackData.questionId);
+    await ctx.reply(answer, {
+      parse_mode: "HTML",
+      disable_web_page_preview: true,
+    });
+    await ctx.answerCallbackQuery();
+    return;
+  }
+
+  if (callbackData.isCorrect) {
+    await ctx.reply("Правильно ✅");
+    await ctx.answerCallbackQuery();
+    return;
+  }
+
+  const answer = getCorrectAnswer(
+    callbackData.type.split("-")[0],
+    callbackData.questionId
+  );
+  await ctx.reply(`Неправильно ❌ Правильный ответ: ${answer}`);
+  await ctx.answerCallbackQuery();
+});
+
+// <==========ERROR HANDLING==========>
+
+bot.catch((err) => {
+  const ctx = err.ctx;
+  console.error(`Error while handling update ${ctx.update.update_id}:`);
+  const e = err.error;
+  if (e instanceof GrammyError) {
+    console.error("Error in request:", e.description);
+  } else if (e instanceof HttpError) {
+    console.error("Could not contact Telegram:", e);
+  } else {
+    console.error("Unknown error:", e);
+  }
+});
+
+bot.start();
